3c2629d35d73d60665e1e1930bfc246d2ecc1e10
hellen-22 <hellenwain@gmail.com>
2024-03-14 16:39:55 +0300

    project setup

---
+++ product/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class AbstractBaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Product(AbstractBaseModel):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='products')
+
+
+    def __str__(self):
+        return self.name
+    
+
+class ProductVariant(AbstractBaseModel):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
+    sku = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    details = models.TextField()
+
+    def __str__(self):
+        return self.name
